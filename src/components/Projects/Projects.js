cee71d1d02e31e8d450a60ca17542e3df751b10b
Sankalp Sharma <sankalpsharma250@gmail.com>
2024-09-28 21:35:24 +0530

    added analytics

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -27,6 +27,7 @@ function Projects() {
               description="A minimalistic to-do list application built with ReactJS. The app allows users to add, edit, and delete tasks with ease. Its clean and intuitive interface enables users to manage their daily tasks efficiently, ensuring a focused and productive workflow. Perfect for those who prefer simplicity, the app helps users track what needs to be done without any distractions."
               ghLink="https://github.com/RIVAL231/To_do_mern"
               demoLink="https://to-do-mern-frontend-ten.vercel.app/"
+              loading="lazy"
             />
           </Col>
 
@@ -38,6 +39,7 @@ function Projects() {
               description="A personal portfolio website built with ReactJS. The website showcases my skills, projects, and blogs. It also provides a brief introduction about me and my journey as a developer. The website is designed to be responsive and user-friendly, ensuring a seamless experience for visitors. It is a reflection of my work and passion for web development."
               ghLink="https://github.com/RIVAL231/Personal_Portfolio"
               demoLink="https://personal-portfolio-sankalp-sharmas-projects.vercel.app/"
+              loading="lazy"
             />
           </Col>
           <Col md={4} className="project-card">
@@ -48,6 +50,7 @@ function Projects() {
               description="A landing page for Hackoff 4.0 built with NextJS. The website provides information about the event, its schedule, and the speakers. It also allows users to register for the event and stay updated with the latest news and announcements. The website is designed to be interactive and engaging, ensuring a memorable experience for participants and attendees."
               ghLink=""
               demoLink="https://hackoff.ietvit.co"
+              loading="lazy"
             />
           </Col>
           <Col md={4} className="project-card">
@@ -58,6 +61,7 @@ function Projects() {
               description="**DigiSwasth** is a web platform designed to enhance healthcare accessibility and efficiency. Built using Next.js and deployed on Vercel, it integrates modern web technologies to offer seamless user interactions. The platform may focus on providing digital health solutions, improving patient management, or offering healthcare services online."
               ghLink="https://github.com/RIVAL231/hackbattle_healthfix"
               demoLink="https://digiswasth-xi.vercel.app/"
+              loading="lazy"
             />
           </Col>
         </Row>
