cc82446e49486a4c6572c2e8b29a57e5c596ef59
Sankalp Sharma <sankalpsharma250@gmail.com>
2024-10-11 23:40:32 +0530

    color changed

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -6,6 +6,7 @@ import project1 from "../../Assets/Projects/project1.png";
 import project2 from "../../Assets/Projects/project2.png";
 import project3 from "../../Assets/Projects/project3.png";
 import project4 from "../../Assets/Projects/project4.png";
+import project5 from "../../Assets/Projects/project5.png";
 
 function Projects() {
   return (
@@ -19,18 +20,31 @@ function Projects() {
           Here are a few projects I've worked on recently.
         </p>
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+         
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={project1}
+              imgPath={project5}
               isBlog={false}
-              title="TaskMaster"
-              description="A minimalistic to-do list application built with ReactJS. The app allows users to add, edit, and delete tasks with ease. Its clean and intuitive interface enables users to manage their daily tasks efficiently, ensuring a focused and productive workflow. Perfect for those who prefer simplicity, the app helps users track what needs to be done without any distractions."
-              ghLink="https://github.com/RIVAL231/To_do_mern"
-              demoLink="https://to-do-mern-frontend-ten.vercel.app/"
+              title="Echoed Words"
+              description="A poetry blog built with NextJS. The blog features a collection of poems written by the author, covering a wide range of themes and emotions. It provides a platform for readers to explore and appreciate the art of poetry and give submissions of their own, offering a unique and engaging experience. The blog is designed to be visually appealing and easy to navigate, ensuring an enjoyable reading experience for visitors."
+              ghLink="https://github.com/RIVAL231/EchoedWords"
+              demoLink="https://echoed-words.vercel.app/"
               loading="lazy"
             />
           </Col>
 
+          
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={project4}
+              isBlog={false}
+              title="DigiSwasth"
+              description="**DigiSwasth** is a web platform designed to enhance healthcare accessibility and efficiency. Built using Next.js and deployed on Vercel, it integrates modern web technologies to offer seamless user interactions. The platform may focus on providing digital health solutions, improving patient management, or offering healthcare services online."
+              ghLink="https://github.com/RIVAL231/hackbattle_healthfix"
+              demoLink="https://digiswasth-xi.vercel.app/"
+              loading="lazy"
+            />
+          </Col>
           <Col md={4} className="project-card">
             <ProjectCard
               imgPath={project2}
@@ -53,14 +67,15 @@ function Projects() {
               loading="lazy"
             />
           </Col>
+          
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={project4}
+              imgPath={project1}
               isBlog={false}
-              title="DigiSwasth"
-              description="**DigiSwasth** is a web platform designed to enhance healthcare accessibility and efficiency. Built using Next.js and deployed on Vercel, it integrates modern web technologies to offer seamless user interactions. The platform may focus on providing digital health solutions, improving patient management, or offering healthcare services online."
-              ghLink="https://github.com/RIVAL231/hackbattle_healthfix"
-              demoLink="https://digiswasth-xi.vercel.app/"
+              title="TaskMaster"
+              description="A minimalistic to-do list application built with ReactJS. The app allows users to add, edit, and delete tasks with ease. Its clean and intuitive interface enables users to manage their daily tasks efficiently, ensuring a focused and productive workflow. Perfect for those who prefer simplicity, the app helps users track what needs to be done without any distractions."
+              ghLink="https://github.com/RIVAL231/To_do_mern"
+              demoLink="https://to-do-mern-frontend-ten.vercel.app/"
               loading="lazy"
             />
           </Col>
